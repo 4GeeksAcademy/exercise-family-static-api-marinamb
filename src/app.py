c6e04220021b1a070ecad1fbbdabe2cb904ec0eb
Marinamb19 <150665821+Marinamb19@users.noreply.github.com>
2024-04-19 18:23:48 +0000

    family-static-marinamb

--- src/app.py
+++ src/app.py
@@ -7,36 +7,56 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
 #from models import Person
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
-
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
-
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
-
 # generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member(id):
+    member = jackson_family.get_member(id)
+    if member is not None :
+        return jsonify({
+            "id" : member["id"],
+            "first_name" : member["first_name"],
+            "last_name" : jackson_family.last_name,
+            "age" : member["age"],
+            "lucky_numbers" : member["lucky_numbers"]
+        }), 200
+    
+@app.route('/member', methods=['POST'])
+def add_member():
+    body = request.get_json(silent = True)
+    id_member = jackson_family._generateId()
+    new_member = {
+        "id" : body["id"],
+        "first_name" : body["first_name"],
+        "last_name" : jackson_family.last_name,
+        "age" : body["age"],
+        "lucky_numbers" : body["lucky_numbers"]
+    }
+    jackson_family.add_member(body)
+    return jsonify({"new_member" : new_member}),200
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    member = jackson_family.delete_member(id)
+    return jsonify({"done" : True}), 200
+
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
-
-    return jsonify(response_body), 200
+    return jsonify(members), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
